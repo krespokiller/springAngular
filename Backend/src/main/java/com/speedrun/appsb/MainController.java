4a54e3b5b0122dfbb8cecf6be2b2bb1ec79d8e13
David Vargas <davidsantiagovargas1234@gmail.com>
2021-10-18 19:16:13 -0500

    cors configurado en springboot

--- Backend/src/main/java/com/speedrun/appsb/MainController.java
+++ Backend/src/main/java/com/speedrun/appsb/MainController.java
@@ -1,20 +1,20 @@
 package com.speedrun.appsb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping(path = "/api")
 public class MainController {
     @Autowired
     private UsuarioRepository usuariorepository;
-
+    
     @PostMapping(path = "/add")
     public @ResponseBody String addUsuario( @RequestParam String REGIdentifica,
                                             @RequestParam String REGNombres,
