3fe1caa713dfc5eab876810a1c091240366c9a5c
David Vargas <davidsantiagovargas1234@gmail.com>
2021-10-23 22:15:02 -0500

    make it better

--- Backend/src/main/resources/static/js/index.js
+++ Backend/src/main/resources/static/js/index.js
@@ -1,12 +1,33 @@
-const url = "http://localhost:8080/api/all";
-(function() {
+const url = "http://localhost:8080/api/add";
+function enviardatos() {
     const xhttp = new XMLHttpRequest;
-    xhttp.open('GET',url,true);
-    xhttp.onreadystatechange = function() {
-        if (this.readyState==4 && this.status==200) {
-            let alldata = JSON.parse(this.responseText);
-            document.getElementById("contenido").textContent=`${alldata.map(x=>x.regnombres)}\n${alldata.map(x=>x.regapellidos)}`
-        }
+
+    let regidentifica = document.forms["formulario"]["regidentifica"].value;
+    let regnombre = document.forms["formulario"]["regnombre"].value;
+    let regapellidos = document.forms["formulario"]["regapellidos"].value;
+    let regcorreo = document.forms["formulario"]["regcorreo"].value;
+    let regmovil = document.forms["formulario"]["regmovil"].value;
+    let regdireccion = document.forms["formulario"]["regdireccion"].value;
+    let regmunicipio = document.forms["formulario"]["regmunicipio"].value;
+        
+    let body ={
+        regidentifica:regidentifica,
+        regnombre:regnombre,
+        regapellidos:regapellidos,
+        regcorreo:regcorreo,
+        regmovil:regmovil,
+        regdireccion:regdireccion,
+        regmunicipio:regmunicipio
     }
-    xhttp.send()
-})();
+
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            console.log("ok")
+        }else{console.error("Error")}
+    }
+
+    xhttp.send(body);
+    console.log(body)
+}
